c3f4c825779ce311627ad89ed6f4e5d2932c2d7e
jack-cummings <jmcummi2@ncsu.edu>
2023-03-02 22:09:18 -0500

    new api in scratch

--- scratch.py
+++ scratch.py
@@ -1,9 +1,16 @@
 import requests
 
-r = requests.post("https://api.deepai.org/api/torch-srgan",
-    data={
-        'image': url,
-    },
-    headers={'api-key': os.environ['deepai_key']}
-)
-print(r.json()['output_url'])
+API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
+headers = {"Authorization": "Bearer ..."}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": "Astronaut riding a horse",
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+image.show()
